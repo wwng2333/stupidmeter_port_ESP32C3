b40dc30e79825c3aa51afe9765c5fb2da648dfdd
Robin Lu <wwng@2333.me>
2023-11-30 11:16:56 +0800

    osTickCount

--- src/main.cpp
+++ src/main.cpp
@@ -331,7 +331,7 @@ void UpdateMsg()
   GenerateMsg("%d", NTP_Count, "NTP");
   GenerateMsg("%d", WiFi.RSSI(), "RSSI");
 
-  GenerateMsg("%lu", osTickCount, "osTickCount");
+  GenerateMsg("%lu", uart_data.osTickCount, "osTickCount");
   GenerateFloatMsg("%.3f", uart_data.mAh, "mAh");
   GenerateFloatMsg("%.3f", uart_data.mWh, "mWh");
   GenerateFloatMsg("%.3f", uart_data.MCU_VCC, "MCU_VCC");
